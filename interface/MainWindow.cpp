d8d95a45bdd69df5a13fe70f55a2dd3a81552525
Caique Reinhold <caiquereinhold@gmail.com>
2014-03-14 16:04:42 -0300

    initial commit

---
+++ interface/MainWindow.cpp
@@ -0,0 +1,62 @@
+#include "MainWindow.h"
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QTextEdit>
+#include <QPushButton>
+
+MainWindow::MainWindow() :
+	QMainWindow()
+{
+	set_up();
+}
+
+MainWindow::~MainWindow()
+{
+	delete central_widget;
+}
+
+void MainWindow::set_up()
+{
+	central_widget = new QWidget(this);
+	v_layout       = new QVBoxLayout();
+	h_layout       = new QHBoxLayout();
+	editor         = new QTextEdit();
+	messages       = new QTextEdit();
+	bt_new      = new QPushButton("new", central_widget);
+    bt_open     = new QPushButton("open", central_widget);
+    bt_save     = new QPushButton("save", central_widget);
+    bt_copy     = new QPushButton("copy", central_widget);
+    bt_paste    = new QPushButton("paste", central_widget);
+    bt_cut      = new QPushButton("cut", central_widget);
+    bt_compile  = new QPushButton("compile", central_widget);
+    bt_gen_code = new QPushButton("generate code", central_widget);
+    bt_team     = new QPushButton("team", central_widget);
+
+	messages->setReadOnly(true);
+	messages->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+	messages->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    messages->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    editor->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    editor->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+	h_layout->addWidget(bt_new);
+	h_layout->addWidget(bt_open);
+	h_layout->addWidget(bt_save);
+	h_layout->addWidget(bt_copy);
+	h_layout->addWidget(bt_paste);
+	h_layout->addWidget(bt_cut);
+	h_layout->addWidget(bt_compile);
+	h_layout->addWidget(bt_gen_code);
+	h_layout->addWidget(bt_team);
+
+	v_layout->addLayout(h_layout);
+	v_layout->addWidget(editor);
+	v_layout->addWidget(messages);
+
+	central_widget->setLayout(v_layout);
+	setCentralWidget(central_widget);
+	resize(1024, 720);
+}
---
+++ interface/MainWindow.h
@@ -0,0 +1,37 @@
+#include <QMainWindow>
+
+class QWidget;
+class QVBoxLayout;
+class QHBoxLayout;
+class QPushButton;
+class QTextEdit;
+
+class MainWindow : public QMainWindow 
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+    ~MainWindow();
+
+private slots:
+
+
+private:
+    QWidget* central_widget;
+    QVBoxLayout* v_layout;
+    QHBoxLayout* h_layout;
+    QTextEdit* editor;
+    QTextEdit* messages;
+    QPushButton* bt_new;
+    QPushButton* bt_open;
+    QPushButton* bt_save;
+    QPushButton* bt_copy;
+    QPushButton* bt_paste;
+    QPushButton* bt_cut;
+    QPushButton* bt_compile;
+    QPushButton* bt_gen_code;
+    QPushButton* bt_team;
+
+    void set_up();
+};
