1a4bb9a3db30dfc09bb880d1efa8dda11c66fab6
Caique Reinhold <caiquereinhold@gmail.com>
2014-03-14 16:10:38 -0300

    correct identation

--- interface/MainWindow.cpp
+++ interface/MainWindow.cpp
@@ -8,24 +8,24 @@
 #include <QPushButton>
 
 MainWindow::MainWindow() :
-	QMainWindow()
+    QMainWindow()
 {
-	set_up();
+    set_up();
 }
 
 MainWindow::~MainWindow()
 {
-	delete central_widget;
+    delete central_widget;
 }
 
 void MainWindow::set_up()
 {
-	central_widget = new QWidget(this);
-	v_layout       = new QVBoxLayout();
-	h_layout       = new QHBoxLayout();
-	editor         = new QTextEdit();
-	messages       = new QTextEdit();
-	bt_new      = new QPushButton("new", central_widget);
+    central_widget = new QWidget(this);
+    v_layout       = new QVBoxLayout();
+    h_layout       = new QHBoxLayout();
+    editor         = new QTextEdit();
+    messages       = new QTextEdit();
+    bt_new      = new QPushButton("new", central_widget);
     bt_open     = new QPushButton("open", central_widget);
     bt_save     = new QPushButton("save", central_widget);
     bt_copy     = new QPushButton("copy", central_widget);
@@ -35,28 +35,28 @@ void MainWindow::set_up()
     bt_gen_code = new QPushButton("generate code", central_widget);
     bt_team     = new QPushButton("team", central_widget);
 
-	messages->setReadOnly(true);
-	messages->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-	messages->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    messages->setReadOnly(true);
+    messages->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    messages->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     messages->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     editor->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     editor->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 
-	h_layout->addWidget(bt_new);
-	h_layout->addWidget(bt_open);
-	h_layout->addWidget(bt_save);
-	h_layout->addWidget(bt_copy);
-	h_layout->addWidget(bt_paste);
-	h_layout->addWidget(bt_cut);
-	h_layout->addWidget(bt_compile);
-	h_layout->addWidget(bt_gen_code);
-	h_layout->addWidget(bt_team);
+    h_layout->addWidget(bt_new);
+    h_layout->addWidget(bt_open);
+    h_layout->addWidget(bt_save);
+    h_layout->addWidget(bt_copy);
+    h_layout->addWidget(bt_paste);
+    h_layout->addWidget(bt_cut);
+    h_layout->addWidget(bt_compile);
+    h_layout->addWidget(bt_gen_code);
+    h_layout->addWidget(bt_team);
 
-	v_layout->addLayout(h_layout);
-	v_layout->addWidget(editor);
-	v_layout->addWidget(messages);
+    v_layout->addLayout(h_layout);
+    v_layout->addWidget(editor);
+    v_layout->addWidget(messages);
 
-	central_widget->setLayout(v_layout);
-	setCentralWidget(central_widget);
-	resize(1024, 720);
+    central_widget->setLayout(v_layout);
+    setCentralWidget(central_widget);
+    resize(1024, 720);
 }
